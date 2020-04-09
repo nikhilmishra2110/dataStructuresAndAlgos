22d7204570824ed3d68672d50c17ae59f1d85ff3
nikhilmishra <nikhil.mishra.2110@gmail.com>
2020-04-10 01:08:27 +0530

    mergeLists.py

---
+++ reverse_string.py
@@ -0,0 +1,19 @@
+from stack import Stack
+
+def reverse_string(input_string):
+    s = Stack()
+
+    '''Loop to push the items into the stack'''
+    for i in range(len(input_string)):
+        s.push(input_string[i])
+
+    reverse_str = ""
+    '''Loop to pop the string in reverse'''
+    while not s.is_empty():
+        reverse_str += s.pop()
+
+    return reverse_str
+
+
+n = "This is India"
+print(reverse_string(n))
