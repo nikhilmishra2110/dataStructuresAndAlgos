ad655b23f886acf59da7ad87283e0663682c3c29
nmishr516 <nikhil_mishra@comcast.com>
2020-04-22 08:12:46 -0400

    Two pointers

--- Algos/Patterns/sliding_window.py
+++ Algos/Patterns/sliding_window.py
@@ -81,18 +81,18 @@ def longest_substring_with_k_distinct(str, k):
     max_length = 0
     # expand the window
     for window_end in range(len(str)):
-        right_char = str[window_end]
-        if right_char not in char_frequency:
-            char_frequency[right_char] = 0
-        char_frequency[right_char]+=1
+        b_pointer = str[window_end]
+        if b_pointer not in char_frequency:
+            char_frequency[b_pointer] = 0
+        char_frequency[b_pointer]+=1
 
     # shrink from left
         while len(char_frequency) > k:
-            left_char = str[window_start]
-            char_frequency[left_char] -= 1
-            if char_frequency[left_char]==0:
-                del char_frequency[left_char]
-            window_start+=1
+            a_pointer = str[window_start]
+            char_frequency[a_pointer] -= 1
+            if char_frequency[a_pointer]==0:
+                del char_frequency[a_pointer]
+            window_start+=1 # shrink the window from left
         max_length = max(max_length, window_end-window_start+1)
     return max_length
 
